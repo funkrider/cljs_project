bbd98a3e4d75bb587e273d49439a3f36eda6cabf
funkrider <ian_davies@hotmail.com>
2017-12-15 17:36:52 -0500

    Change to use piggieback nREPL.

--- project.clj
+++ project.clj
@@ -3,6 +3,10 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.671"]]
+  :plugins [[cider/cider-nrepl "0.15.1"]]
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.2"]
+                                  [org.clojure/tools.nrepl "0.2.10"]]
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :source-paths ["src" "target/classes"]
   :clean-targets ["out" "release"]
